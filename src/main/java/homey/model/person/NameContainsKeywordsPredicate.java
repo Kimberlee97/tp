679c969818894c39e2637f5be94e0bbdd16ae426
zhiyu05 <kohzhiyu5@gmail.com>
2025-10-08 15:54:12 +0800

    Change name search to support partial keyword matching

--- src/main/java/homey/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/homey/model/person/NameContainsKeywordsPredicate.java
@@ -1,44 +1,58 @@
 package homey.model.person;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.function.Predicate;
 
-import homey.commons.util.StringUtil;
 import homey.commons.util.ToStringBuilder;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ * Matching is case-insensitive and allows partial keyword matches
  */
 public class NameContainsKeywordsPredicate implements Predicate<Person> {
-    private final List<String> keywords;
+    private final List<String> keywordsLowerCased;
 
+    /**
+     * Constructs a NameContainsKeywordsPredicate with the given list of keywords.
+     * All keywords are converted to lowercase for case-insensitive matching.
+     * @param keywords the list of keywords to match against; must not be null
+     * @throws NullPointerException if keywords is null
+     */
     public NameContainsKeywordsPredicate(List<String> keywords) {
-        this.keywords = keywords;
+        requireNonNull(keywords);
+        this.keywordsLowerCased = keywords.stream()
+                .map(k -> k == null ? "" : k.toLowerCase(Locale.ROOT))
+                .toList();
     }
 
     @Override
     public boolean test(Person person) {
-        return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+        String name = person.getName().fullName.toLowerCase(Locale.ROOT);
+        for (String k : keywordsLowerCased) {
+            if (!k.isEmpty() && name.contains(k)) {
+                return true; // match partial keywords
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
+        return other == this
+                || (other instanceof NameContainsKeywordsPredicate)
+                && keywordsLowerCased.equals(((NameContainsKeywordsPredicate) other).keywordsLowerCased);
 
-        // instanceof handles nulls
-        if (!(other instanceof NameContainsKeywordsPredicate)) {
-            return false;
-        }
-
-        NameContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (NameContainsKeywordsPredicate) other;
-        return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
     }
 
     @Override
+    public int hashCode() {
+        return keywordsLowerCased.hashCode();
+    }
+    @Override
     public String toString() {
-        return new ToStringBuilder(this).add("keywords", keywords).toString();
+        return new ToStringBuilder(this).add("keywords", keywordsLowerCased).toString();
     }
 }
