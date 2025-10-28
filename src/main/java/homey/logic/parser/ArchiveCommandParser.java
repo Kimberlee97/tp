561db72046ac728ee0ff6044e8cfc5e2db801693
JiaYuan <hjyadm@gmail.com>
2025-10-29 00:02:17 +0800

    Fix error message for unclear archive / unarchive

--- src/main/java/homey/logic/parser/ArchiveCommandParser.java
+++ src/main/java/homey/logic/parser/ArchiveCommandParser.java
@@ -1,6 +1,9 @@
 package homey.logic.parser;
 
+import static homey.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import homey.commons.core.index.Index;
+import homey.commons.exceptions.IllegalValueException;
 import homey.logic.commands.ArchiveCommand;
 import homey.logic.parser.exceptions.ParseException;
 
@@ -13,7 +16,12 @@ public class ArchiveCommandParser implements Parser<ArchiveCommand> {
     @Override
     public ArchiveCommand parse(String args) throws ParseException {
         final String trimmed = args == null ? "" : args.trim();
-        final Index index = ParserUtil.parseIndex(trimmed);
-        return new ArchiveCommand(index);
+        try {
+            Index index = ParserUtil.parseIndex(trimmed);
+            return new ArchiveCommand(index);
+        } catch (IllegalValueException e) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ArchiveCommand.MESSAGE_USAGE), e);
+        }
     }
 }
