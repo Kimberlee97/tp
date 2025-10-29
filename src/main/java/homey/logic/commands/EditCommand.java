131dd88eb00f87fbeb13016d75ecaa8f22360e25
Tan Yun Qi <tan_yun_qi@hotmail.sg>
2025-10-29 14:56:31 +0800

    Edit relation command execution logic

--- src/main/java/homey/logic/commands/EditCommand.java
+++ src/main/java/homey/logic/commands/EditCommand.java
@@ -184,6 +184,7 @@ public class EditCommand extends Command {
         private Phone phone;
         private Email email;
         private Address address;
+        private Relation relation;
         private TransactionStage stage;
         private Remark remark;
         private Set<Tag> tags;
@@ -247,6 +248,14 @@ public class EditCommand extends Command {
 
         public Optional<Address> getAddress() {
             return Optional.ofNullable(address);
+        }
+
+        public void setRelation(Relation relation) {
+            this.relation = relation;
+        }
+
+        public Optional<Relation> getRelation() {
+            return Optional.ofNullable(relation);
         }
 
         public void setStage(TransactionStage stage) {
