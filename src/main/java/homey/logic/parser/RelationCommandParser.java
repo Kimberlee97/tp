5881d3f9125c155671ef3a0d846cd3acf8470144
Tan Yun Qi <tan_yun_qi@hotmail.sg>
2025-10-29 14:41:37 +0800

    Add logging

--- src/main/java/homey/logic/parser/RelationCommandParser.java
+++ src/main/java/homey/logic/parser/RelationCommandParser.java
@@ -56,6 +56,8 @@ public class RelationCommandParser implements Parser<RelationCommand> {
             relation = ParserUtil.parseRelation("");
         }
 
+        logger.fine("Creating Relation Command: " + index.getOneBased() + " " + relation.value);
+
         return new RelationCommand(index, relation);
     }
 }
