bccaca02698b3fcae4d4f499943c72bccde10c5a
Tan Yun Qi <tan_yun_qi@hotmail.sg>
2025-10-29 14:32:10 +0800

    Fix relation parsing error message

--- src/main/java/homey/logic/parser/RelationCommandParser.java
+++ src/main/java/homey/logic/parser/RelationCommandParser.java
@@ -5,6 +5,11 @@ import static homey.logic.parser.CliSyntax.PREFIX_CLIENT;
 import static homey.logic.parser.CliSyntax.PREFIX_VENDOR;
 import static java.util.Objects.requireNonNull;
 
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+import homey.MainApp;
+import homey.commons.core.LogsCenter;
 import homey.commons.core.index.Index;
 import homey.commons.exceptions.IllegalValueException;
 import homey.logic.commands.RelationCommand;
@@ -15,6 +20,10 @@ import homey.model.tag.Relation;
  * Parses input arguments and creates a new {@code RelationCommand} object.
  */
 public class RelationCommandParser implements Parser<RelationCommand> {
+    private Index index;
+    private Relation relation;
+    private static final Logger logger = LogsCenter.getLogger(MainApp.class);
+
     /**
      * Parses the given {@code String} of arguments in the context of the {@code RelationCommand}
      * and returns a {@code RelationCommand} object for execution.
@@ -25,21 +34,18 @@ public class RelationCommandParser implements Parser<RelationCommand> {
         args = args.toLowerCase();
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_CLIENT, PREFIX_VENDOR);
 
-        if (argMultimap.getValue(PREFIX_VENDOR).isEmpty() && argMultimap.getValue(PREFIX_CLIENT).isEmpty()
-                || argMultimap.getValue(PREFIX_VENDOR).isPresent() && argMultimap.getValue(PREFIX_CLIENT).isPresent()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    RelationCommand.MESSAGE_USAGE));
-        }
-
-        Index index;
         try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            index = ParserUtil.parseIndex(argMultimap.getPreamble().split("\\s+")[0]);
         } catch (IllegalValueException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     RelationCommand.MESSAGE_USAGE), ive);
         }
 
-        Relation relation;
+        if (argMultimap.getValue(PREFIX_VENDOR).isEmpty() && argMultimap.getValue(PREFIX_CLIENT).isEmpty()
+                || argMultimap.getValue(PREFIX_VENDOR).isPresent() && argMultimap.getValue(PREFIX_CLIENT).isPresent()) {
+            relation = ParserUtil.parseRelation("");
+        }
+
         if (argMultimap.getValue(PREFIX_CLIENT).isPresent()
                 && argMultimap.getValue(PREFIX_CLIENT).get().isEmpty()) {
             relation = ParserUtil.parseRelation("client");
